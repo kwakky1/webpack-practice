262d73fe72bcb999ea54e892a789c8a4aad6d0ea
andy <kwakky1@gmail.com>
2021-08-13 11:40:29 +0900

    웹팩으로 css loader 적용하기 webpack.config.js에 https://webpack.js.org/loaders/css-loader/ 해당 주소에서 어떻게 css 파일을 웹팩으로 로드 할 수 있는지 설명해준다.

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 }
